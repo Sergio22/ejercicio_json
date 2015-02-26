e25a268dd87948042ae157a12ac1d29b1cd02c57
Sergio <sergiomedranocastro@gmail.com>
2015-02-26 19:54:42 +0100

    Prueba hastag

--- ejerciciojson.py
+++ ejerciciojson.py
@@ -3,8 +3,12 @@
 import json
 
 
+
 with open ('mifile.json') as data_file:
 	data = json.load(data_file)
 
 for tuits in data:
-	print tuits['created_at']	
+	hastag_array = tuits['entities']['hashtags']
+	for h in hastag_array:
+		print h['text']
+		
